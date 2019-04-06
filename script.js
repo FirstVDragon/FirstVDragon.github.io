fdf65a83a3bc065bc494e99ddf940c27182ecf88
First Dragon <firstvdragon@gmail.com>
2019-04-06 19:31:25 +0300

    Initial commit

--- script.js
+++ script.js
@@ -23,3 +23,39 @@ $(document).keypress(function (e) {
        }
     }
 });
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        events: {
+            'onReady': onPlayerReady
+        },
+        playerVars: 
+          {
+            listType:'playlist',
+            list: 'RDYFi4FTOuqKk'
+          }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+/* debugger */;
+    num = _.random(0, 40);
+  
+  setTimeout(() => {
+  player.playVideoAt(num);
+  }, 1000);
+}
